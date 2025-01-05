96bd941a4702d32e549bd05e3feb53f61df3e7bd
ajaybhuj24 <ajaybhuj2456.com>
2025-01-05 17:07:28 +0545

    creating ecommerce , navbar created and also the react-router-dom has been used to navigate to different pages

---
+++ frontend/src/pages/Collection.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Collection = () => {
+  return <div>collection pager</div>;
+};
+
+export default Collection;
---
+++ frontend/src/pages/Product.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Product = () => {
+  return <div>products page</div>;
+};
+
+export default Product;
