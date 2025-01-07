a5e8b4b16648c5945ff1f137cc0e7643e696ec66
ajaybhuj24 <bhujajay9@gmail.com>
2025-01-07 20:06:27 +0545

    bestseller components created

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Hero from "../components/Hero";
 import LatestCollection from "../components/LatestCollection";
+import BestSeller from "../components/BestSeller";
 const Home = () => {
   return (
     <div className="">
       <Hero />
       <LatestCollection />
+      <BestSeller />
     </div>
   );
 };
